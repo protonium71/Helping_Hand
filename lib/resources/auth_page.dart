a4263c6c67f00f36764cc27c314171f8a3ae8e89
codor07 <srivastavaprashant829@gmail.com>
2023-11-09 10:52:25 +0530

    connected navbar to routes and removed minor bugs

--- lib/resources/auth_page.dart
+++ lib/resources/auth_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:helping_hand/views/user/login_or_register.dart';
+import 'package:helping_hand/views/user/navigation.dart';
 import 'package:helping_hand/views/user/user_feed.dart';
 
 class AuthPage extends StatelessWidget {
@@ -14,7 +15,7 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot) {
           //user is logged in
           if(snapshot.hasData){
-            return const UserFeed();
+            return const Navigation();
           }
           //user is not logged in
           else{
